093ab402052977c4e8a033359e178f71c4608d6f
Aleksei Burov <burov_alexey@mail.ru>
2020-06-05 17:44:58 +0500

    Интерфейс вызова функции и передачи параметров через SwaggerAPI

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         'aiohttp',
         'environs',
+        'aiohttp-swagger',
     ],
     url='',
     author='Aleksei Burov',
--- treeviewer/service.py
+++ treeviewer/service.py
@@ -5,6 +5,7 @@
 import logging
 
 from aiohttp import web
+from aiohttp_swagger import setup_swagger
 
 import treeviewer
 from treeviewer.misc import log, settings
@@ -12,21 +13,87 @@ from treeviewer.misc import log, settings
 logger = logging.getLogger(__name__)
 
 
-def create_app():
-    """Возвращает экземпляр приложения."""
-    return web.Application()
+async def root_handler(request):
+    """Redirect to the swagger api of the service."""
+    raise web.HTTPFound(location=settings.ENV('SWAGGER_URL'))
 
 
-async def handle(request):
+async def tree_handler(request):
+    """Show tree.
+    ---
+    description: This end-point shows a data tree by your parameters.
+    tags:
+    - Show tree
+    produces:
+    - text/plain
+    parameters:
+    - name: root_id
+      in: query
+      required: false
+      type: integer
+      description: ID of the node from which the search begins
+    - name: sort_fld
+      in: query
+      required: true
+      type: string
+      description: Sort field
+      enum:
+        - id
+        - parеnt_id
+        - title
+        - registered_in
+    - name: sort_dir
+      in: query
+      required: true
+      type: string
+      description: Sort direction (asc/dsc)
+      enum:
+        - asc
+        - dsc
+    - name: depth
+      in: query
+      required: false
+      type: integer
+      description: Sample size
+      default: 1
+      enum: [1, 2, 3, 4, 5]
+    responses:
+      200:
+        description: Successful operation. Returns database output for the given parameters
+      400:
+        description: Invalid request data
+    """
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
 
 
+async def ping_handler(request):
+    """Health check.
+    ---
+    description: This end-point allow to test that service is up.
+    tags:
+    - Health check
+    produces:
+    - text/plain
+    responses:
+        "200":
+            description: successful operation. Return "pong" text
+        "405":
+            description: invalid HTTP Method
+    """
+    return web.Response(text="pong")
+
+
+def create_app():
+    """Возвращает экземпляр приложения."""
+    return web.Application()
+
+
 def setup_routes(app):
-    # TODO: (burov_alexey@mail.ru 5 июн. 2020 г. 15:54:47)
-    # Нужен редирект корня на swagger
-    app.router.add_get('/tree', handle)
+    app.router.add_route('GET', '/', root_handler)
+    app.router.add_route('GET', '/tree', tree_handler)
+    app.router.add_route('GET', "/ping", ping_handler)
 
 
 def main():
@@ -35,6 +102,7 @@ def main():
     log.setup_root_logger(settings.ENV('LOG_LEVEL'))
     app = create_app()
     setup_routes(app)
+    setup_swagger(app, swagger_url=settings.ENV('SWAGGER_URL'), ui_version=2)
     web.run_app(app, host=settings.ENV('APP_HOST'),
                      port=settings.ENV('APP_PORT'))
 
